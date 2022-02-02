d9e6b8bf16262c3762c3200cf436028c03d69846
baluczak <bartosz.luczak@btc-ag.com>
2022-02-02 10:50:37 +0100

    Added redis data model

---
+++ api/Models/RedisDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ServerManagementSystem.Models
+{
+    public class RedisDataModel
+    {
+        public string TimeStamp { get; set; }
+        public string KeyName { get; set; }
+        public object Data { get; set; }
+    }
+}
